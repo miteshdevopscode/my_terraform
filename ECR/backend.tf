fb486724a869bc92853fbc9fc535f4ea77f0ccf8
miteshglober <mitesh.jayaswal@gmail.com>
2019-02-28 09:12:46 +0000

    adding ecr again

--- ECR/backend.tf
+++ ECR/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "tfstate2019"
-    key     = "tfstates/myterraform-remote-states-for-ECR"
+    key     = "tfstates/myterraform-remote-states-for-ECS"
     region  = "us-east-1"
     profile = "default"
   }
